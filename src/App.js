08f13dc2e82e3d730de6fb6d47280780e3434bc0
John Mateer <jj.mateer@live.com>
2019-10-19 16:47:40 -0700

    Set up react app, added header and section.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./components/header";
+import Section from "./components/Section"
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Section/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Section.css";
+
+function Section() {
+    return (
+        <div className="Section">
+            
+        </div>
+    );
+  }
+  
+  export default Section;
