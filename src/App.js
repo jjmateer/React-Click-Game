8e5976ce1ba88e05162152479052e9375e850759
John Mateer <jj.mateer@live.com>
2019-10-21 14:28:39 -0700

    Changed name of score.json to userInfo.json. Added a second object to the array which I used in the selectTile function to change it to correct or incorrect.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import Header from "./components/header";
 import Section from "./components/Section"
 import Tile from "./components/Tile"
 import tiles from "./tiles.json";
-import scores from "./score.json";
+import userInfo from "./userInfo.json";
 import "./styles/App.css";
 
 class App extends React.Component {
   state = {
-    tiles, scores
+    tiles, userInfo
   };
   selectTile = id => {
     const tilesFilter = this.state.tiles.filter(tile => tile.id === id);
     if (tilesFilter[0].selected === false) {
+      this.state.userInfo[1].greeting = "Correct"
       tilesFilter[0].selected = true;
-      this.state.scores[0].score++
-      if (this.state.scores[0].score > this.state.scores[0].highScore) {
-        this.state.scores[0].highScore++
+      this.state.userInfo[0].score++
+      if (this.state.userInfo[0].score > this.state.userInfo[0].highScore) {
+        this.state.userInfo[0].highScore++
       }
     } else {
       for (let i = 0; i < this.state.tiles.length - 1; i++) {
         this.state.tiles[i].selected = false;
       }
-      this.state.scores[0].score = 0;
+      this.state.userInfo[1].greeting = "Incorrect"
+      this.state.userInfo[0].score = 0;
     };
     this.shuffle();
   };
@@ -33,12 +35,12 @@ class App extends React.Component {
       [tempArr[i], tempArr[j]] = [tempArr[j], tempArr[i]];
     }
     this.setState({ tiles: tempArr });
-    console.log(this.state.tiles)
+    // console.log(this.state.tiles)
   };
   render() {
     return (
       <div>
-        <Header />
+        <Header/>
         <Section>
           {this.state.tiles.map(tile => (
             <Tile
--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Score from "./score"
-import scores from "../score.json";
+import userInfo from "../userInfo.json";
 import "../styles/App.css";
 function Header() {
+    
     return (
         <header className="header">
             <div className="scoreHolder">
-                <h1>React Click game</h1>
+                <h1 id='logo'>React Click game</h1>
 
                 <div id="userAlert">
-
+                {userInfo[1].greeting}
                 </div>
                 <div id="scoreDiv">
-                    <div className="scoreDisplays">Current Score: {scores[0].score}</div>
-                    <div className="scoreDisplays">High Score: {scores[0].highScore}</div>
+                    <div className="scoreDisplays">Current Score: {userInfo[0].score}</div>
+                    <div className="scoreDisplays">High Score: {userInfo[0].highScore}</div>
                 </div>
             </div>
         </header>
