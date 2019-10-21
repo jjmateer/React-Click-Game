ab7e2b3118dc8e8fd2eaccf83e149e8393470785
John Mateer <jj.mateer@live.com>
2019-10-20 22:36:03 -0700

    Deleted the file for the score component, put content in the header file. Added div which I will use to alert the user if theyare correct or incorrect.

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Score from "./score"
+import scores from "../score.json";
 import "../styles/App.css";
 function Header() {
     return (
         <header className="header">
-            <h1>React Click game</h1>
-                <Score/>
+            <div className="scoreHolder">
+                <h1>React Click game</h1>
+
+                <div id="userAlert">
+
+                </div>
+                <div id="scoreDiv">
+                    <div className="scoreDisplays">Current Score: {scores[0].score}</div>
+                    <div className="scoreDisplays">High Score: {scores[0].highScore}</div>
+                </div>
+            </div>
         </header>
     );
-  }
-  
-  export default Header;
-  
+}
+
+export default Header;
