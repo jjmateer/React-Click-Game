3d1cba1959a786a62043406355eb8a16cb3db787
John Mateer <jj.mateer@live.com>
2019-10-21 23:40:36 -0700

    Fixed the direct state mutation errors by using setState() to update properties of objects in the arrays.

--- src/App.js
+++ src/App.js
@@ -13,48 +13,72 @@ class App extends React.Component {
   selectTile = id => {
     const tilesFilter = this.state.tiles.filter(tile => tile.id === id);
     if (tilesFilter[0].selected === false) {
-      this.state.userInfo[1].greeting = "Correct"
+      this.setState(prevState => ({
+        userInfo: [
+          ...prevState.userInfo,
+          prevState.userInfo[1].greeting = "Correct",
+          prevState.userInfo[0].score += 1
+        ]
+      }))
       tilesFilter[0].selected = true;
-      this.state.userInfo[0].score++
-      if (this.state.userInfo[0].score > this.state.userInfo[0].highScore) {
-        this.state.userInfo[0].highScore++
+      if ( this.state.userInfo[0].score > this.state.userInfo[0].highScore) {
+        // this.state.userInfo[0].highScore++
+        this.setState(prevState => ({
+          userInfo: [
+            ...prevState.userInfo,
+            prevState.userInfo[0].highScore = this.state.userInfo[0].score
+          ]
+        }))
       }
     } else {
       for (let i = 0; i < this.state.tiles.length - 1; i++) {
-        this.state.tiles[i].selected = false;
+        this.setState(prevState => ({
+          tiles: [
+            ...prevState.tiles,
+            prevState.tiles[i].selected = false
+          ]
+        }))
       }
-      this.state.userInfo[1].greeting = "Incorrect"
-      this.state.userInfo[0].score = 0;
-    };
+
+      this.setState(prevState => ({
+        userInfo: [
+          ...prevState.userInfo,
+          prevState.userInfo[1].greeting = "Incorrect",
+          prevState.userInfo[0].score = 0
+        ]
+      }))
+    // this.state.userInfo[1].greeting = "Incorrect"
+    // this.state.userInfo[0].score = 0;
+  };
     this.shuffle();
   };
-  shuffle = () => {
-    let tempArr = this.state.tiles.slice();
-    for (let i = tempArr.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [tempArr[i], tempArr[j]] = [tempArr[j], tempArr[i]];
-    }
-    this.setState({ tiles: tempArr });
-    // console.log(this.state.tiles)
-  };
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Section>
-          {this.state.tiles.map(tile => (
-            <Tile
-              id={tile.id}
-              image={tile.image}
-              selected={tile.selected}
-              selectTile={this.selectTile}
-              shuffle={this.shuffle}
-            />
-          ))}
-        </Section>
-      </div>
-    );
+shuffle = () => {
+  let tempArr = this.state.tiles.slice();
+  for (let i = tempArr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [tempArr[i], tempArr[j]] = [tempArr[j], tempArr[i]];
   }
+  this.setState({ tiles: tempArr });
+  // console.log(this.state.tiles)
+};
+render() {
+  return (
+    <div>
+      <Header />
+      <Section>
+        {this.state.tiles.map(tile => (
+          <Tile
+            id={tile.id}
+            image={tile.image}
+            selected={tile.selected}
+            selectTile={this.selectTile}
+            shuffle={this.shuffle}
+          />
+        ))}
+      </Section>
+    </div>
+  );
+}
 }
 
 export default App;
